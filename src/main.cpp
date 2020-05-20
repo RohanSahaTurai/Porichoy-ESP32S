aa0b49e31e4ec8cd7bb8506a5d2a6564c9ff68aa
RohanSahaTurai <rohansahaturai@gmail.com>
2020-05-20 18:25:23 +1000

    Face recognition results received and trigger activited

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 //Maximum packet size = 50 KiB
 #define MQTT_MAX_PACKET_SIZE 51200
 #define MQTT_KEEPALIVE 3600
-#define MQTT_SOCKET_TIMEOUT 3600
+#define MQTT_SOCKET_TIMEOUT 1
 #include <PubSubClient.h>
 
 const char* WIFI_SSID     = "Rohan Hotspot";
@@ -17,14 +17,51 @@ const char* ServerIP   = "142.93.62.203";
 const char* ClientID   = "Rohan-ESP32CAM";
 const int   PortServer =  1883;
 
-const char* MQTT_SUB_TOPIC = "";
+const char* MQTT_SUB_TOPIC = "Result";
 const char* MQTT_PUB_TOPIC = "Image";
 
+const int QoS = 1;
+
+//callback function prototype
+void callback(char* topic, byte* payload, unsigned int length);
+
 WiFiClient ESPClient;
-PubSubClient MQTTClient(ServerIP, PortServer, ESPClient);
+PubSubClient MQTTClient(ServerIP, PortServer, callback, ESPClient);
 
 const int CaptureDistance = 40;   //40cm
 const int SampleTime = 500;       //500ms
+const int ACK_TIMEOUT = 30000;     //30000ms = 30s
+
+bool Callback_Requested = false;
+bool Callback_Handled  = false;
+
+// Call back function
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.println("Message Arrived");
+
+  // Don't process the callback if not requested.
+  // This protects from unwanted messages received
+  if (!Callback_Requested)
+    return;
+
+  // Handle the callback for each topic
+  if (!strcmp(topic, MQTT_SUB_TOPIC))
+  {
+    std::string response((char*)payload, length);
+
+    Serial.println("Response: " + String(response.c_str()));
+
+    if (!response.find("NO MATCH" ))
+      LED_On(LED_RED);
+    
+    else
+      LED_On(LED_GREEN);
+  }
+
+  // set the flag to indicate callback handled
+  Callback_Handled = true;
+}
 
 void WiFi_Connect()
 {
@@ -48,6 +85,8 @@ void WiFi_Connect()
 void MQTT_Connect()
 {   
     MQTTClient.disconnect();
+
+    MQTTClient.setCallback(callback);
     
     Serial.print("Connecting to MQTT broker on ");
     Serial.println(ServerIP);
@@ -67,6 +106,18 @@ void MQTT_Connect()
             delay(5000);            
         }
     }
+
+    // Subscribe to receive acknowledgements
+    Serial.println("Subscribing to Topic: " + String(MQTT_SUB_TOPIC));
+
+    while (!MQTTClient.subscribe(MQTT_SUB_TOPIC, QoS))
+    {
+      Serial.print(".");
+      delay(500);
+    }
+
+    Serial.println("Suscribed Successfully");
+
 }
 
 void setup() 
@@ -112,7 +163,7 @@ void loop()
   // Check if distance is valid
   if (distance <= CaptureDistance && distance != -1)
   {
-      LED_On(LED_YELLOW);
+      LED_On(LED_WHITE);
 
       if (Camera_Capture(&fb))
       {
@@ -128,16 +179,41 @@ void loop()
         // Check if the the publishing is finsied
         if (MQTTClient.endPublish())
            Serial.println("Frame Sent!");
-  
+        
+        // Request and poll for a callback
+        Callback_Requested = true;
+        unsigned long int startTime = millis();
+
         Camera_FreeFrameBuffer(&fb);
+
+        // Wait for acknowledgement upto the timeout period
+        Serial.println("Waiting for response...");
+        while(!Callback_Handled)
+        {
+          MQTTClient.loop();
+
+          //after timeout turn LED Red
+          if (millis() - startTime >= ACK_TIMEOUT)
+          {
+            Serial.println("Timeout...");
+            LED_On(LED_RED);
+            break;
+          }
+        }
+
+        // reset the flag
+        Callback_Handled = false;
+        Callback_Requested = false;
 
         // delay at least 5 seconds before next frame is captured
         delay(5000);
       }
   }
 
+  MQTTClient.loop();
+
   // Sampling time of 500ms
   delay(SampleTime);
 
-  LED_On(LED_WHITE);
+  LED_On(LED_AQUA);
 }
